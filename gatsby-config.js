8f439bf4274c84f6f7baaaf65d6cef233c72faba
Rob Upchurch <rlupchurch3@gmail.com>
2022-12-23 14:44:07 -0500

    Fixed Ventura link and site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 /**
  * @type {import('gatsby').GatsbyConfig}
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Kerakis`,
+    siteTitleAlt: `Kerakis - My Lair`,
+    siteHeadline: `Kerakis - My Lair`,
+    siteUrl: `https://kerakis.online`,
+    siteDescription: `A homepage for all things Kerakis.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Kerakis`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -60,4 +60,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
